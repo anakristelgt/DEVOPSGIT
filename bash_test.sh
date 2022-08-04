8e51a7e472ee15aff37082e6614717b4fa049a34
AnaKGonzalez <ana.kristel@hotmail.com>
2022-08-04 04:20:48 +0000

    Deleted Files

--- bash_test.sh
+++
@@ -1,6 +0,0 @@
-#! /bin/bash
-
-# Este archivo limpia el file mock_data
-echo "Este archivo lo creó $USER"
-tail -n+2 MOCK_DATA.csv |  awk -F, '{print "\"" $3 "\",", "\"" $4 "\",", "\""$5"\""}' | sort -k 1 -n | sed 's/ //g' > sort.txt
-echo "Listo."
